3db20222d4122501cd9b73f8748c3514a83129d5
Patrick G <geek1011@outlook.com>
2018-11-06 17:12:39 -0500

    Simplified and improved performance of test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,31 +1,32 @@
 #!/bin/bash -e
 require() { command -v "$1" &>/dev/null || { echo >&2 "Error: This script requires the command $1."; exit 1; }; }
 
-require xz
-require zip
-require tar
-require grep
+require kobopatch
 require sed
 
-testtemp="$PWD/testtemp"
-rm -rf "$testtemp"
-mkdir "$testtemp"
+versions=src/versions/*/
+if [[ ! -z "$1" ]]; then
+    versions=src/versions/$1/
+fi
 
-for f in build/kobopatch_*.zip; do
-    version="$(echo "$f" | grep -Eo '[0-9.]+[0-9]+')"
-    printf "Testing built patches for version %s\n" "$version"
-    
-    temp="$testtemp/$version"
-    mkdir "$temp"
-
-    echo "--> Unzipping patch zip"
-    unzip -qq "$f" -d "$temp"
-
-    kobopatch -tf "testdata/$version.tar.xz" "$temp/kobopatch_$version/kobopatch.yaml" || { echo "Test failed for $version! Stopping."; rm -rf "$temp" "$testtemp"; exit 1; }
-
-    rm -rf "$temp"
-    printf "\n"
+for f in $versions; do
+    version="$(basename "$f")"
+    printf "Testing patches for %s\n" "$version"
+    (
+        echo "version: 4.11.11980";
+        echo "in: /dev/null";
+        echo "out: /dev/null";
+        echo "log: /dev/null";
+        echo "patchFormat: kobopatch";
+        echo "patches:";
+        for pf in src/versions/$version/*/*.yaml; do
+            binary="$(basename "$(dirname "$pf")" | sed "s/.yaml//g")"
+            creator="$(basename "$pf" | sed "s/.yaml//g")"
+            echo "  src/versions/$version/$binary.yaml/$creator.yaml: usr/local/Kobo/$binary"
+        done
+    ) | kobopatch -tf "testdata/$version.tar.xz" -  || {
+        echo "Test failed for $version! Stopping.";
+        rm -rf "$temp" "$testtemp";
+        exit 1;
+    }
 done
-
-echo "Cleaning up"
-rm -rf "$testtemp"
